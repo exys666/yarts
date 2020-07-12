b3997662008b2c608445ba664a6e207ac4621b74
exys666 <exys666@gmail.com>
2020-07-12 14:35:08 +0200

    Init project

---
+++ src/main/java/exys666/yarts/YetAnotherRealTimeStatisticsApplication.java
@@ -0,0 +1,13 @@
+package exys666.yarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherRealTimeStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetAnotherRealTimeStatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exys666/yarts/YetAnotherRealTimeStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package exys666.yarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherRealTimeStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
